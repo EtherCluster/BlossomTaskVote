87d5dcdd787c5b0cb3f7a513058cba8b3dc14698
EtherCluster <ethercluster@protonmail.com>
2021-12-03 08:24:36 +0200

    GM MetaVerse

---
+++ blossomupvoter/src/components/Hello.js
@@ -0,0 +1,14 @@
+
+function Hello() {
+  return (
+    <div className="Hello">
+     
+               <p>
+          GM MetaVerse!
+         </p>
+      
+    </div>
+  );
+}
+
+export default Hello;
